9c7e68341319e00eed0351cfcdc7c531e6610aed
Luke Child <lukechildmusic@gmail.com>
2020-11-24 14:13:27 +0000

    Removed unncessary stuff and began adding project pages

---
+++ js/navbar.js
@@ -0,0 +1,64 @@
+var isToggled = false;
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() 
+{
+
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+
+  
+}
+
+
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+
+} 
+
+
+function toggleNavBar()
+{
+  switch(isToggled)
+  {
+    case true:
+      closeNav();
+      isToggled = false;
+      break;
+    
+    case false:
+      openNav()
+      isToggled = true;
+      break;
+  }
+
+ 
+}
+
+function menuButton(x) {
+  x.classList.toggle("change");
+}
+
+function changeImageDisplayType()
+{
+  
+  if(window.innerWidth < 601)
+  {
+    document.getElementById("picture").classList.remove("w3-display-middle");
+  }
+
+  if(window.innerWidth > 601)
+  {
+    document.getElementById("picture").classList.add("w3-display-middle");
+  }
+
+  
+
+  
+}
+
+
