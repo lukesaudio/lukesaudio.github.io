bf0098107d87774810adbde1c45a2e08f4731912
Luke Child <40809085+lukesaudio@users.noreply.github.com>
2022-06-23 22:25:31 +0100

    New Fresh Website Template
    We doing it properly this time baby

--- websiteScript.js
+++ websiteScript.js
@@ -1,15 +1,35 @@
-
-  /* Open when someone clicks on the span element */
-function openNav() 
+function bounceThenSize()
 {
+  window.onload = function()
+  {
+                setTimeout(function()
+                {
+                    var x = document.getElementsByClassName("icon");
+                    for(var i = 0; i < x.length; i++)
+                    {
+                      x[i].classList.remove("animate__bounce");
+                    }
+                    console.log(x);
+                }, 1000);
+
+            }
+}
+
+
+function iconNavText()
+{
+  var x = document.getElementById("iconLabel");
+  
+
+}
+
+/* Open when someone clicks on the span element */
+function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() 
-{
+function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
-
-//-------------------------------------------------------------------------------SLIDESHOW JS
 
