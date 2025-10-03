2530c926f89f540a443a165c8c4211111384188f
Luke Child <40809085+lukesaudio@users.noreply.github.com>
2025-10-03 17:00:24 +0100

    svg bug

--- nerdfarters/2025/3/sketch.js
+++ nerdfarters/2025/3/sketch.js
@@ -51,11 +51,11 @@ function preload()
 {
 
   background  = loadImage("background.jpeg")
-  cloud1 = loadImage('/clouds/cloud1.png');
-  cloud2 = loadImage('/clouds/cloud2.png');
-  cloud3 = loadImage('/clouds/cloud3.png');
-  cloud4 = loadImage('/clouds/cloud4.png');
-  cloud5 = loadImage('/clouds/cloud5.png');
+  cloud1 = loadImage('/cloudImages/cloud1.png');
+  cloud2 = loadImage('/cloudImages/cloud2.png');
+  cloud3 = loadImage('/cloudImages/cloud3.png');
+  cloud4 = loadImage('/cloudImages/cloud4.png');
+  cloud5 = loadImage('/cloudImages/cloud5.png');
 
   cloud1pos = -109;
   cloud2pos = -131;
